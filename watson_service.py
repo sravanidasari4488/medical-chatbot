d0370896ef32ce801c40d0522b5853f7e103fee2
sravanidasari4488 <154404196+sravanidasari4488@users.noreply.github.com>
2025-07-08 22:30:08 +0530

    Update watson_service.py

--- watson_service.py
+++ watson_service.py
@@ -5,6 +5,8 @@ import streamlit as st
 from typing import Dict, Any
 from google import genai
 from google.genai import types
+from ibm_watson_machine_learning.foundation_models import Model
+from ibm_watson_machine_learning import APIClient
 
 class MedicalChatbot:
     def __init__(self):
